4af43d5d474ac186a4c851b7b768f7cc3e973e2b
Paulius Osipauskas <pauosi@ktu.lt>
2024-11-14 15:37:18 +0200

    config tests

---
+++ Snake-Tests.Tests/BundleConfigTests.cs
@@ -0,0 +1,79 @@
+﻿using NUnit.Framework;
+using System.Web.Optimization;
+using SignalR_Snake;
+using System.Linq;
+
+namespace SignalR_Snake.Tests
+{
+    [TestFixture]
+    public class BundleConfigTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            BundleTable.Bundles.Clear(); // Clear any previous bundles to ensure test isolation
+        }
+
+        [Test]
+        public void RegisterBundles_ShouldAddJQueryBundle()
+        {
+            var bundles = new BundleCollection();
+
+            BundleConfig.RegisterBundles(bundles);
+
+            var jqueryBundle = bundles.GetBundleFor("~/bundles/jquery");
+            Assert.IsNotNull(jqueryBundle, "jQuery bundle should be registered.");
+            Assert.IsTrue(jqueryBundle is ScriptBundle, "jQuery bundle should be a ScriptBundle.");
+            Assert.IsTrue(jqueryBundle.Transforms.Any(transform => transform is JsMinify), "jQuery bundle should use JsMinify.");
+        }
+
+        [Test]
+        public void RegisterBundles_ShouldAddJQueryValidationBundle()
+        {
+            var bundles = new BundleCollection();
+
+            BundleConfig.RegisterBundles(bundles);
+
+            var jqueryValBundle = bundles.GetBundleFor("~/bundles/jqueryval");
+            Assert.IsNotNull(jqueryValBundle, "jQuery validation bundle should be registered.");
+            Assert.IsTrue(jqueryValBundle is ScriptBundle, "jQuery validation bundle should be a ScriptBundle.");
+            Assert.IsTrue(jqueryValBundle.Transforms.Any(transform => transform is JsMinify), "jQuery validation bundle should use JsMinify.");
+        }
+
+        [Test]
+        public void RegisterBundles_ShouldAddModernizrBundle()
+        {
+            var bundles = new BundleCollection();
+
+            BundleConfig.RegisterBundles(bundles);
+
+            var modernizrBundle = bundles.GetBundleFor("~/bundles/modernizr");
+            Assert.IsNotNull(modernizrBundle, "Modernizr bundle should be registered.");
+            Assert.IsTrue(modernizrBundle is ScriptBundle, "Modernizr bundle should be a ScriptBundle.");
+        }
+
+        [Test]
+        public void RegisterBundles_ShouldAddBootstrapBundle()
+        {
+            var bundles = new BundleCollection();
+
+            BundleConfig.RegisterBundles(bundles);
+
+            var bootstrapBundle = bundles.GetBundleFor("~/bundles/bootstrap");
+            Assert.IsNotNull(bootstrapBundle, "Bootstrap bundle should be registered.");
+            Assert.IsTrue(bootstrapBundle is ScriptBundle, "Bootstrap bundle should be a ScriptBundle.");
+        }
+
+        [Test]
+        public void RegisterBundles_ShouldAddCssBundle()
+        {
+            var bundles = new BundleCollection();
+
+            BundleConfig.RegisterBundles(bundles);
+
+            var cssBundle = bundles.GetBundleFor("~/Content/css");
+            Assert.IsNotNull(cssBundle, "CSS bundle should be registered.");
+            Assert.IsTrue(cssBundle is StyleBundle, "CSS bundle should be a StyleBundle.");
+        }
+    }
+}
---
+++ Snake-Tests.Tests/RouteConfigTests.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+using Moq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using SignalR_Snake;
+
+namespace SignalR_Snake.Tests
+{
+    [TestFixture]
+    public class RouteConfigTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            RouteTable.Routes.Clear();
+        }
+
+        [Test]
+        public void RegisterRoutes_ShouldIgnoreAxdRequests()
+        {
+            var routes = new RouteCollection();
+            RouteConfig.RegisterRoutes(routes);
+
+            // Use the correct format with leading slash ('/')
+            var context = new Mock<HttpContextBase>();
+            context.Setup(c => c.Request.AppRelativeCurrentExecutionFilePath).Returns("/resource.axd/somepath");
+
+            RouteData routeData = routes.GetRouteData(context.Object);
+
+            // Assert that no route matches the .axd path
+            Assert.IsNull(routeData, "The .axd path should be ignored by the routing configuration.");
+        }
+
+
+        [Test]
+        public void RegisterRoutes_DefaultRoute_ShouldMapToHomeIndex()
+        {
+            var routes = new RouteCollection();
+            RouteConfig.RegisterRoutes(routes);
+
+            var context = new Mock<HttpContextBase>();
+            context.Setup(c => c.Request.AppRelativeCurrentExecutionFilePath).Returns("~/");
+
+            RouteData routeData = routes.GetRouteData(context.Object);
+
+            Assert.IsNotNull(routeData, "The default route should match the root URL.");
+            Assert.AreEqual("Home", routeData.Values["controller"], "Default controller should be 'Home'.");
+            Assert.AreEqual("Index", routeData.Values["action"], "Default action should be 'Index'.");
+            Assert.AreEqual(UrlParameter.Optional, routeData.Values["id"], "Default id should be optional.");
+        }
+    }
+}
