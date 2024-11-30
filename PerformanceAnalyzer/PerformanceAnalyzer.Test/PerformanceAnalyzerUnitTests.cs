fdb594d1ec16d1fd20ac1ffeda7819c15445dbbe
Martynas Kuliešius <martiss200@gmail.com>
2024-11-30 21:31:09 +0200

    LETS FUCKING EXPLODE THIS PIECE OF PROGRAM XDDDDDDDD
    package pridetas lmao

---
+++ PerformanceAnalyzer/PerformanceAnalyzer.Test/PerformanceAnalyzerUnitTests.cs
@@ -0,0 +1,163 @@
+﻿using Microsoft.CodeAnalysis.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+using VerifyCS = PerformanceAnalyzer.Test.CSharpCodeFixVerifier<
+    PerformanceAnalyzer.PerformanceAnalyzerAnalyzer,
+    PerformanceAnalyzer.PerformanceAnalyzerCodeFixProvider>;
+
+namespace PerformanceAnalyzer.Test
+{
+    [TestClass]
+    public class PerformanceAnalyzerUnitTests
+    {
+        // Test for Class Naming Rule
+        [TestMethod]
+        public async Task Test_ClassNamingRule()
+        {
+            var test = @"
+namespace TestNamespace
+{
+    class {|#0:testClass|} // Class name should be capitalized
+    {
+    }
+}";
+
+            var fixedCode = @"
+namespace TestNamespace
+{
+    class TESTCLASS
+    {
+    }
+}";
+
+            var expected = new DiagnosticResult(PerformanceAnalyzerAnalyzer.ClassNamingRule)
+                .WithLocation(0)
+                .WithArguments("testClass");
+
+            await VerifyCS.VerifyCodeFixAsync(test, expected, fixedCode);
+        }
+
+        // Test for Uncached Count Rule
+        [TestMethod]
+        public async Task Test_UncachedCountRule()
+        {
+            var test = @"
+using System.Collections.Generic;
+
+namespace TestNamespace
+{
+    class TestClass
+    {
+        public void TestMethod()
+        {
+            var list = new List<int>();
+            for (int i = 0; i < list.Count; i++) // Count is not cached
+            {
+            }
+        }
+    }
+}";
+
+            var fixedCode = @"
+using System.Collections.Generic;
+
+namespace TestNamespace
+{
+    class TestClass
+    {
+        public void TestMethod()
+        {
+            var list = new List<int>();
+            var cachedCount = list.Count; // Cached Count
+            for (int i = 0; i < cachedCount; i++)
+            {
+            }
+        }
+    }
+}";
+
+            var expected = new DiagnosticResult(PerformanceAnalyzerAnalyzer.UncachedCountRule)
+                .WithLocation(0);
+
+            await VerifyCS.VerifyCodeFixAsync(test, expected, fixedCode);
+        }
+
+        // Test for Excessive Parameters Rule
+        [TestMethod]
+        public async Task Test_ExcessiveParametersRule()
+        {
+            var test = @"
+namespace TestNamespace
+{
+    class TestClass
+    {
+        public void TestMethod(int a, int b, int c, int d, int e, int f) // Too many parameters
+        {
+        }
+    }
+}";
+
+            var fixedCode = @"
+namespace TestNamespace
+{
+    class TestClassParameters
+    {
+        public int A { get; set; }
+        public int B { get; set; }
+        public int C { get; set; }
+        public int D { get; set; }
+        public int E { get; set; }
+        public int F { get; set; }
+    }
+
+    class TestClass
+    {
+        public void TestMethod(TestClassParameters parameters)
+        {
+        }
+    }
+}";
+
+            var expected = new DiagnosticResult(PerformanceAnalyzerAnalyzer.ExcessiveParametersRule)
+                .WithLocation(0);
+
+            await VerifyCS.VerifyCodeFixAsync(test, expected, fixedCode);
+        }
+
+        // Test for Missing XML Documentation Rule
+        [TestMethod]
+        public async Task Test_MissingXmlDocumentationRule()
+        {
+            var test = @"
+namespace TestNamespace
+{
+    class TestClass
+    {
+        public void TestMethod() // No XML documentation
+        {
+        }
+    }
+}";
+
+            var fixedCode = @"
+namespace TestNamespace
+{
+    class TestClass
+    {
+        /// <summary>
+        /// Describe the purpose of this method.
+        /// </summary>
+        public void TestMethod()
+        {
+        }
+    }
+}";
+
+            var expected = new DiagnosticResult(PerformanceAnalyzerAnalyzer.MissingXmlDocRule)
+                .WithLocation(0)
+                .WithArguments("TestMethod");
+
+            await VerifyCS.VerifyCodeFixAsync(test, expected, fixedCode);
+        }
+    }
+}
---
+++ PerformanceAnalyzer/PerformanceAnalyzer/PerformanceAnalyzerAnalyzer.cs
@@ -0,0 +1,139 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System.Collections.Immutable;
+using System.Linq;
+
+/*
+ Instructions for updating package:
+change .csproj version
+build on release
+dotnet build -c Release
+dotnet pack -c Release
+dotnet nuget push C:\Users\marti\Desktop\GItHub\Snake-Testavimas\PerformanceAnalyzer\PerformanceAnalyzer.Package\bin\Release\PerformanceAnalyzer.x.x.0.nupkg -k oy2fajj3ecxlytirpqpqixkhbh5ilj5ujddyg2pwgmbyo4 -s https://api.nuget.org/v3/index.json
+
+ 
+ 
+ 
+ */
+
+namespace PerformanceAnalyzer
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class PerformanceAnalyzerAnalyzer : DiagnosticAnalyzer
+    {
+        public const string ClassNamingDiagnosticId = "PerformanceAnalyzer_ClassNaming";
+        public const string UncachedCountDiagnosticId = "PerformanceAnalyzer_UncachedCount";
+        public const string ExcessiveParametersDiagnosticId = "PerformanceAnalyzer_ExcessiveParameters";
+        public const string MissingXmlDocDiagnosticId = "PerformanceAnalyzer_MissingXmlDoc";
+
+        public static readonly DiagnosticDescriptor ClassNamingRule = new DiagnosticDescriptor(
+            ClassNamingDiagnosticId,
+            "Class naming issue",
+            "Class '{0}' should be renamed to follow naming conventions.",
+            "Naming",
+            DiagnosticSeverity.Warning,
+            isEnabledByDefault: true);
+
+        public static readonly DiagnosticDescriptor UncachedCountRule = new DiagnosticDescriptor(
+            UncachedCountDiagnosticId,
+            "Uncached collection.Count in loop",
+            "Consider caching collection.Count outside the loop for better performance.",
+            "Performance",
+            DiagnosticSeverity.Warning,
+            isEnabledByDefault: true);
+
+        public static readonly DiagnosticDescriptor ExcessiveParametersRule = new DiagnosticDescriptor(
+            ExcessiveParametersDiagnosticId,
+            "Method has too many parameters",
+            "Method '{0}' has too many parameters. Consider reducing the number for better maintainability.",
+            "Performance",
+            DiagnosticSeverity.Info,
+            isEnabledByDefault: true);
+
+        public static readonly DiagnosticDescriptor MissingXmlDocRule = new DiagnosticDescriptor(
+            MissingXmlDocDiagnosticId,
+            "Missing XML documentation",
+            "Method '{0}' lacks XML documentation.",
+            "Documentation",
+            DiagnosticSeverity.Info,
+            isEnabledByDefault: true);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(
+            ClassNamingRule,
+            UncachedCountRule,
+            ExcessiveParametersRule,
+            MissingXmlDocRule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+
+            // Register analysis actions
+            context.RegisterSyntaxNodeAction(AnalyzeClassNaming, SyntaxKind.ClassDeclaration);
+            context.RegisterSyntaxNodeAction(AnalyzeUncachedCount, SyntaxKind.ForStatement);
+            context.RegisterSyntaxNodeAction(AnalyzeMethodParameters, SyntaxKind.MethodDeclaration);
+            context.RegisterSyntaxNodeAction(AnalyzeMissingXmlDoc, SyntaxKind.MethodDeclaration);
+        }
+
+        private void AnalyzeClassNaming(SyntaxNodeAnalysisContext context)
+        {
+            var classDeclaration = (ClassDeclarationSyntax)context.Node;
+            if (classDeclaration.Identifier.Text.Any(char.IsLower))
+            {
+                var diagnostic = Diagnostic.Create(ClassNamingRule, classDeclaration.Identifier.GetLocation(), classDeclaration.Identifier.Text);
+                context.ReportDiagnostic(diagnostic);
+            }
+        }
+
+        private void AnalyzeUncachedCount(SyntaxNodeAnalysisContext context)
+        {
+            var forStatement = (ForStatementSyntax)context.Node;
+
+            // Check if the loop condition contains collection.Count
+            if (forStatement.Condition is BinaryExpressionSyntax condition &&
+                condition.Right is MemberAccessExpressionSyntax memberAccess &&
+                memberAccess.Name.Identifier.Text == "Count")
+            {
+                // Report a diagnostic if Count is used without caching
+                var diagnostic = Diagnostic.Create(UncachedCountRule, memberAccess.GetLocation());
+                context.ReportDiagnostic(diagnostic);
+            }
+        }
+
+        private void AnalyzeMethodParameters(SyntaxNodeAnalysisContext context)
+        {
+            var methodDeclaration = (MethodDeclarationSyntax)context.Node;
+
+            // Check for excessive parameters (e.g., more than 5)
+            if (methodDeclaration.ParameterList.Parameters.Count > 5)
+            {
+                var diagnostic = Diagnostic.Create(
+                    ExcessiveParametersRule,
+                    methodDeclaration.Identifier.GetLocation(),
+                    methodDeclaration.Identifier.Text);
+                context.ReportDiagnostic(diagnostic);
+            }
+        }
+
+        private void AnalyzeMissingXmlDoc(SyntaxNodeAnalysisContext context)
+        {
+            var methodDeclaration = (MethodDeclarationSyntax)context.Node;
+
+            // Check for XML documentation above the method
+            var trivia = methodDeclaration.GetLeadingTrivia();
+            var hasXmlDoc = trivia.Any(t => t.Kind() == SyntaxKind.SingleLineDocumentationCommentTrivia);
+
+            if (!hasXmlDoc)
+            {
+                var diagnostic = Diagnostic.Create(
+                    MissingXmlDocRule,
+                    methodDeclaration.Identifier.GetLocation(),
+                    methodDeclaration.Identifier.Text);
+                context.ReportDiagnostic(diagnostic);
+            }
+        }
+    }
+}
